9de91d3693a756af93e24232ee86c76646c07361
0ranko0P <ranko0p@outlook.com>
2020-07-06 01:54:07 +0800

    sdm660-common: Set zygote preforking in system_prop
    Move it to PRODUCT_SYSTEM_DEFAULT_PROPERTIES to fix violated by
    allow vendor_init device_config_runtime_native_prop:property_service { set };
    
    Test: boot and getprop
    Test: logcat USAP pool

--- properties.mk
+++ properties.mk
@@ -209,5 +209,5 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.debug.wfd.enable=1
 
 # Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
